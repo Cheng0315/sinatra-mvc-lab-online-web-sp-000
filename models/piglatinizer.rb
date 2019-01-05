d7588c5ac0fc9a7cc573e2ec28b25c08c01f76b9
Cheng Vue <cvu3@ucsc.edu>
2019-01-05 06:51:05 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,15 @@
+class piglatinizer
+  attr_accessor :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def latinize
+    words_arr = @text.split(" ")
+    words_arr.map do |word|
+      first_char = word.!slice[0, 1]
+      word + first_char + "ay"
+    end.join(" ")
+  end
+end
