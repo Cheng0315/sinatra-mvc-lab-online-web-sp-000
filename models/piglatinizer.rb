7300da7b577991cf3635e747fc4fea2e2467b860
Cheng Vue <cvu3@ucsc.edu>
2019-01-05 07:30:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,16 +5,18 @@ class PigLatinizer
     words_arr.map do |word|
       if word.length == 1
         word + "way"
-      elsif word.downcase == "pork"
-        first_char = word.slice!(0, 1)
-        word + first_char + "ay"
-      elsif word[0].downcase == 'p'
-        first_two_chars = word.slice!(0, 2)
-        word + first_two_chars + "ay"
       else
-        first_char = word.slice!(0, 1)
+        slice_at_idx = find_first_vowel_idx(word)
+        first_char = word.slice!(0, slice_at_idx)
         word + first_char + "ay"
       end
     end.join(" ")
+  end
+
+  def find_first_vowel_idx(word)
+    vowels = "aeiou"
+    word.downcase.each do |char, idx|
+      return idx if vowels.include?(char)
+    end
   end
 end
