7db565910df2e18926f0db89e708af99f22d1f48
Cheng Vue <cvu3@ucsc.edu>
2019-01-05 07:11:40 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,6 +5,9 @@ class PigLatinizer
     words_arr.map do |word|
       if word.length == 1
         word + "way"
+      elsif word[0].downcase == 'p'
+        first_two_chars = word.slice!(0, 2)
+        word + first_two_chars + "ay"
       else
         first_char = word.slice!(0, 1)
         word + first_char + "ay"
