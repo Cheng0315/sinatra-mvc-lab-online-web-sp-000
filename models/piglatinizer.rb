700516623e36abf80946bd19c46943f5cbc9ce14
Cheng Vue <cvu3@ucsc.edu>
2019-01-05 07:05:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,9 @@
 class PigLatinizer
 
-  def piglatinize(word)
-    words_arr = word.split(" ")
+  def piglatinize(words)
+    words_arr = words.split(" ")
     words_arr.map do |word|
-      first_char = word.!slice(0, 1)
+      first_char = word.slice!(0, 1)
       word + first_char + "ay"
     end.join(" ")
   end
